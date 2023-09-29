95c2906a83baa3c53846b61f628570d1791ce000
valentin <valentin.udrea1@yahoo.com>
2023-09-29 21:04:43 +0300

    Finished Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+public class Calculator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1 for Addition");
+        System.out.println("2 for Substraction");
+        System.out.println("3 for multiplication");
+        System.out.println("4 for division");
+
+        int choose = input.nextInt();
+
+        System.out.println("Chose two numbers");
+        double firstNum = input.nextDouble();
+        double secondNum = input.nextDouble();
+
+        if (choose == 1){
+            System.out.println("Adding the two numbers = " + add(firstNum,secondNum));
+        }else if( choose == 2){
+            System.out.println("Substracting the two numbers = " + sub(firstNum,secondNum));
+        }else if( choose == 3){
+            System.out.println("Mutiplying the two numbers = " + mult(firstNum,secondNum));
+        }else if( choose == 4){
+            System.out.println("Dividing the two numbers = " + div(firstNum,secondNum));
+        }
+
+
+
 
         }
+        public static double add(double x,double y){
+        double result = x + y;
+        return result;
+
+        }
+    public static double sub(double x,double y){
+        double result = x - y;
+        return result;
+
     }
+    public static double mult(double x,double y){
+        double result = x * y;
+        return result;
+    }
+    public static double div(double x,double y){
+        double result = x / y;
+        return result;
+
+    }
+
 }
